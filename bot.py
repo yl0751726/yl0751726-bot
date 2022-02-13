89dce9c46912c2923c24e32270328decb08247f1
yl0751726 <86944289+yl0751726@users.noreply.github.com>
2022-02-14 00:51:12 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
 import discord
 from discord.ext import commands
-
+import json
+import random
+import os
+#開啟setting.json並用json.load開啟 將開啟的檔案設為jdata
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+#機器人指令前餟
 bot = commands.Bot(command_prefix='[')
-
-TOKEN = 'OTQxNzIzMDI4NDAyODc2NTI2.YgaF6g.gB53404ay_jqKAJYVlm4XmwQBLQ'
-JOIN = '867789793440825346'
-LEAVE = '867789793440825346'
-
+#機器人準備完成提示
 @bot.event
 async def on_ready():
     print("準備完成")
-#成員加入
-@bot.event
-async def on_memer_join(member):
-    channel = bot.get_channel(JOIN)
-    await channel.send(f'阿呦~這麼好喔 {member} 加入了~!')
-#成員離開
-@bot.event
-async def on_memer_join(member):
-    channel = bot.get_channel(LEAVE)
-    await channel.send(f'阿呦~ {member} 離開了~真是太遜了!')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'真是太遜了!目前延遲:{round(bot.latency*1000)}毫秒')
-
-bot.run(TOKEN)
+#讀取檔案,第二行為確認導入文件是否為.py文件,第三行為省略.py副檔名
+for filename in os.listdir('./cmds.'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+#確認須執行模組與執行模組是否相同,下行為啟動機器人
+if __name__=="__main__":
+    bot.run(jdata['TOKEN'])
